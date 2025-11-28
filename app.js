620b8cc93d6f9486f67df12eb2c4efc209083409
Sinéad McIntyre <shins.mcintyre@gmail.com>
2025-11-28 13:47:53 +0000

    feat: css styling

--- app.js
+++ app.js
@@ -22,21 +22,18 @@ const imageData = [
   },
 ];
 
-console.log(imageData.imageSrc);
 // STEP 2: Create thumbnail images (and store in container)=============
 
-function createThumbnails() {
+function createThumbnails(test) {
   // select the dom element to contain the thumbnails
   const thumbnailContainer = document.getElementById("thumbnail-container");
   // this is a repetitive task --> use a for loop(for, forEach...)
   // Loop task:
   for (let i = 0; i < imageData.length; i++) {
-    // imageData.forEach(function () {
     // - create img element
     const thumbnailImg = document.createElement("img");
 
     // update src and alt attributes(properties) of the img element to match those in the array (parameters)
-    // Q - is this how you attribute from the array?? Check the array workshop
     thumbnailImg.src = imageData[i].imageSrc;
     thumbnailImg.alt = imageData[i].imageAlt;
 
@@ -44,35 +41,36 @@ function createThumbnails() {
     thumbnailImg.className = "thumbnail-img";
 
     // -add an event listener to each image to make them clickable --> the event handler of this event is the function you write to create the full screen images (STEP 3)
-    // const clickBtn = document.createElement("click-btn");
-    thumbnailImg.addEventListener("click", createFullscreenImages);
+    thumbnailImg.addEventListener("click", function () {
+      createFullscreenImages(i);
+    });
     // - append created images to the thumbnail-container
     thumbnailContainer.appendChild(thumbnailImg);
-    console.log(thumbnailImg);
   }
 }
 
 // STEP 3: Create fullscreen images =====================================
 
-function createFullscreenImages() {
+function createFullscreenImages(i) {
   // this is the event handler
   // select the fullscreen container
   const fullScreenContainer = document.getElementById("fullscreen-container");
+
   // delete the current fullscreen image --> fullScreenContainer.innerHTML = null
   fullScreenContainer.innerHTML = null;
 
-  for (let i = 0; i < imageData.length; i++) {
-    // create image
-    const fullScreenImg = document.createElement("img");
-    // update it's values  (properties) - from the same array
-    fullScreenImg.src = imageData[i].imageSrc;
-    fullScreenImg.alt = imageData[i].imageAlt;
-    // add className for styling (making the image full screen)
-    fullScreenImg.className = "fullscreen-img";
-    // append image to the container
-    fullScreenContainer.appendChild(fullScreenImg);
-  }
+  // create image
+  const fullScreenImg = document.createElement("img");
+  // update it's values  (properties) - from the same array
+  // use parameters to set out which image it should relate to
+  fullScreenImg.src = imageData[i].imageSrc;
+  fullScreenImg.alt = imageData[i].imageAlt;
+  // add className for styling (making the image full screen)
+  fullScreenImg.className = "fullscreen-img";
+  // append image to the container
+  fullScreenContainer.appendChild(fullScreenImg);
 }
+
 // add the createFullScreenImages function as the event handler of the event above
 // invoke / call the createThumbnails function
 
